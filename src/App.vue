393f223f437e699415ee7ac7cd081d8737471915
FridaHaggstromIths <frida.haggstrom@iths.se>
2024-02-09 12:11:39 +0100

    Navbar

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomeView from './views/HomeView.vue'
+import NavComponent from "./components/NavComponent.vue"
+</script>
+
+<template>
+  <NavComponent />
+  <HomeView />
+</template>
+
+<style scoped>
+</style>
